7459994587e2d2289f715361e22f78fc4f7cee4e
bayamasa <mapi0213mgt@gmail.com>
2022-01-25 22:35:07 +0900

    MOD: success compile

--- philo_d/src/main.c
+++ philo_d/src/main.c
@@ -6,14 +6,16 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 13:44:56 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/01/25 17:21:01 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/01/25 22:29:00 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void *thread_func(t_status *stat)
+void *thread_func(void *stat)
 {
+	t_status *s;
+	s = (t_status *)stat;
 	return (0);
 }
 
@@ -30,7 +32,7 @@ int	main(int argc, char const *argv[])
 		return (EXIT_FAILURE);
 	while (i < stat.philo_num)
 	{
-		status = pthread_create(&thread, NULL, thread_func, &stat);
+		status = pthread_create(&thread, NULL, thread_func, (&stat));
 	}
 	
 	return (0);
