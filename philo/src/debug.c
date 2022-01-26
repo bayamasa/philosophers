c5eb1ff95a3849f5cfd1200c8af508483c38d65e
bayamasa <mapi0213mgt@gmail.com>
2022-01-26 14:08:27 +0900

    MOD: コンパイルエラー修正

--- philo/src/debug.c
+++ philo/src/debug.c
@@ -6,7 +6,7 @@
 /*   By: mhirabay <mhirabay@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 10:48:56 by mhirabay          #+#    #+#             */
-/*   Updated: 2022/01/26 10:57:57 by mhirabay         ###   ########.fr       */
+/*   Updated: 2022/01/26 14:02:56 by mhirabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@ void	print_all_fork_status(t_philo_attr *a)
 	{
 		if (a->is_fork_taken[i] == false)
 		{
-			printf("is_fork_taken[%d] = %s\n", i, a->is_fork_taken[i], "false");
+			printf("is_fork_taken[%zu] = %s\n", i,"false");
 		}
 		else if (a->is_fork_taken[i] == true)
 		{
-			printf("is_fork_taken[%d] = %s\n", i, a->is_fork_taken[i], "true");
+			printf("is_fork_taken[%zu] = %s\n", i,"true");
 		}
 		else
 		{
-			printf("is_fork_taken[%d] = %s\n", i, a->is_fork_taken[i], "undefined");
+			printf("is_fork_taken[%zu] = %s\n", i,"undefined");
 		}
 		i++;
 	}
