b4f847a37cfc54d162bcbd2b6e87be16b48c7f53
bayamasa <mapi0213mgt@gmail.com>
2022-01-31 13:28:17 +0900

    ADD: mutexとdata raceの関係を検証

---
+++ sandbox/test_mutex_double.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+typedef struct t_mutex
+{
+	pthread_mutex_t test_1;
+	pthread_mutex_t test_2;
+	int				status;
+}	m;
+// mutexを2つの位置
+// 別々のmutex構造体でlockをかけると-fsanitize=threadでコンパイルしたときにdata raceが発生する。
+// 同じmutexでlockをかけたら大丈夫(それはそう)
+
+void *test_1(void *params)
+{
+	m *p;
+	p = (m *)params;
+	pthread_mutex_lock(&(p->test_1));
+	printf("test_1 kita\n");
+	sleep(5);
+	p->status = 100;
+	printf("p->status test_1 = %d\n", p->status);
+	pthread_mutex_unlock(&(p->test_1));
+	return (NULL);
+}
+
+void *test_2(void *params)
+{
+	m *p;
+	p = (m *)params;
+	pthread_mutex_lock(&(p->test_2));
+	printf("test_2 kita\n");
+	sleep(1);
+	p->status = 111;
+	printf("p->status test_2 = %d\n", p->status);
+	pthread_mutex_unlock(&(p->test_2));
+	return (NULL);
+}
+
+
+
+int	main()
+{
+	pthread_t	thread_1;
+	pthread_t	thread_2;
+
+	m	params;
+	
+	params.status = 100;
+	pthread_mutex_init(&params.test_1, NULL);
+	pthread_mutex_init(&params.test_2, NULL);
+
+	if (pthread_create(&thread_1, NULL, test_1, &params) != 0)
+	{
+		printf("dame\n");
+	}
+
+	usleep(50);
+	if (pthread_create(&thread_2, NULL, test_2, &params) != 0)
+	{
+		printf("dame\n");
+	}
+	//
+	sleep(7);
+	pthread_join(thread_1, NULL);
+	pthread_join(thread_2, NULL);
+	return (0);
+}
